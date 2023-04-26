1e7553abbf44c4118c1be9370d3a22af0dcd16e1
robert.cho <robert.cho@samsung.com>
2023-04-26 00:40:09 +0000

    fix : add assertion for per_atom mean and delete rewritten inherited function

--- src/modules/normalizer.py
+++ src/modules/normalizer.py
@@ -1,8 +1,5 @@
 """
-Copyright (c) Facebook, Inc. and its affiliates.
-
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
+written by robert.cho and byunggook.na (SAIT)
 """
 
 import torch
@@ -12,21 +9,12 @@ class NormalizerPerAtom(Normalizer):
     """Normalize a Tensor and restore it later."""
 
     def __init__(self, tensor=None, mean=None, std=None, device=None):
+        assert tensor is not None, "tensor should be None when NormalizerPerAtom is used"
         super().__init__(tensor,mean,std,device)
 
-    def to(self, device):
-        self.mean = self.mean.to(device)
-        self.std = self.std.to(device)
 
     def norm(self, tensor,N):
         return (tensor - self.mean*N) / self.std
 
     def denorm(self, normed_tensor,N):
         return normed_tensor * self.std + self.mean*N
-
-    def state_dict(self):
-        return {"mean": self.mean, "std": self.std}
-
-    def load_state_dict(self, state_dict):
-        self.mean = state_dict["mean"].to(self.mean.device)
-        self.std = state_dict["std"].to(self.mean.device)
