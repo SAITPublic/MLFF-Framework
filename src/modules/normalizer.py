46a379f34d15bac4b9890668400b076c8d71e51d
robert.cho <robert.cho@samsung.com>
2023-04-26 01:06:38 +0000

    fix: bug fix for wrong assertion in normalizer

--- src/modules/normalizer.py
+++ src/modules/normalizer.py
@@ -9,7 +9,7 @@ class NormalizerPerAtom(Normalizer):
     """Normalize a Tensor and restore it later."""
 
     def __init__(self, tensor=None, mean=None, std=None, device=None):
-        assert tensor is not None, "tensor should be None when NormalizerPerAtom is used"
+        assert tensor is None, "tensor should be None when NormalizerPerAtom is used"
         super().__init__(tensor,mean,std,device)
 
 
