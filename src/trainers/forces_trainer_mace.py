048eecd1b4954db11d06e9779048de4cc38bb39d
byunggook.na <byunggook.na@samsung.com>
2023-05-18 11:01:49 +0900

    fix minor bugs

--- src/trainers/forces_trainer_mace.py
+++ src/trainers/forces_trainer_mace.py
@@ -43,7 +43,12 @@ class MACEForcesTrainer(ForcesTrainer):
     def _parse_config(self, config):
         trainer_config = super()._parse_config(config)
 
-        if not trainer_config["dataset"].get("normalize_labels", True):
+        if trainer_config["dataset"].get("normalize_labels", True):
+            no_scale = trainer_config["model_attributes"].get("scaling", "rms_forces_scaling") == "no_scaling"
+            no_shift = not trainer_config["model_attributes"].get("shifting", False)
+            if no_scale and no_shift:
+                raise AttributeError("Please make the normalization option consistent. Change normalize_labels = False, or use scale or shift")
+        else:
             bm_logging.info("Applying the data normalization is default in MACE, but the normalization turns off in this training")
         
         # MACE does not need normalizer (they use own normaliation strategy)
@@ -164,36 +169,31 @@ class MACEForcesTrainer(ForcesTrainer):
         bm_logging.info(f"Average number of neighbors: {avg_num_neighbors}")
 
         # 4. set scaling and shifting
-        if trainer_config["dataset"].get("normalize_labels", True):
-            if "scaling_type" not in mace_statistics:
-                scaling = self.config["model_attributes"].get("scaling", "rms_forces_scaling")
-                if scaling == "no_scaling":
-                    mean, std = 0.0, 1.0
-                    bm_logging.info("No scaling (and neither shifting) selected")
-                elif scaling == "std_scaling":
-                    mean, std = compute_mean_std_atomic_inter_energy(
-                        data_loader=self.train_loader,
-                        atomic_energies=np.array(self.atomic_energies),
-                    )
-                elif scaling == "rms_forces_scaling":
-                    mean, std = compute_mean_rms_energy_forces(
-                        data_loader=self.train_loader,
-                        atomic_energies=np.array(self.atomic_energies),
-                    )
-                else:
-                    raise RuntimeError(f"{scaling} is not supported")
-                mace_statistics["scaling_type"] = scaling
-                mace_statistics["mean"] = mean
-                mace_statistics["std"] = std
+        scaling = self.config["model_attributes"].get("scaling", "rms_forces_scaling")
+        if "scaling_type" not in mace_statistics:
+            if scaling == "no_scaling":
+                mean, std = 0.0, 1.0
+                bm_logging.info("No scaling (and neither shifting) selected")
+            elif scaling == "std_scaling":
+                mean, std = compute_mean_std_atomic_inter_energy(
+                    data_loader=self.train_loader,
+                    atomic_energies=np.array(self.atomic_energies),
+                )
+            elif scaling == "rms_forces_scaling":
+                mean, std = compute_mean_rms_energy_forces(
+                    data_loader=self.train_loader,
+                    atomic_energies=np.array(self.atomic_energies),
+                )
             else:
-                if scaling != mace_statistics["scaling_type"]:
-                    raise RuntimeError(f"There is difference between `scaling_type` in this training config and `scaling_type` saved in {mace_statistics_file_path}")
-                mean = mace_statistics["mean"]
-                std = mace_statistics["std"]
+                raise RuntimeError(f"{scaling} is not supported")
+            mace_statistics["scaling_type"] = scaling
+            mace_statistics["mean"] = mean
+            mace_statistics["std"] = std
         else:
-            mean = 0.0
-            std = 1.0
-            bm_logging.info("By setting `normalize_labels = False`, it is identical that no scaling (and neither shifting) selected")
+            if scaling != mace_statistics["scaling_type"]:
+                raise RuntimeError(f"There is difference between `scaling_type` in this training config and `scaling_type` saved in {mace_statistics_file_path}")
+            mean = mace_statistics["mean"]
+            std = mace_statistics["std"]
 
         if not self.config["model_attributes"].get("shifting", False):
             # MACE : no shifting
