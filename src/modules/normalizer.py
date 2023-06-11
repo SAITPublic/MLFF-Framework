a8c2839bfc33ea0bf574ebde08b19cbfc3399165
byunggook.na <byunggook.na@samsung.com>
2023-06-11 20:54:16 +0900

    fix a log issue of normalizer

--- src/modules/normalizer.py
+++ src/modules/normalizer.py
@@ -32,9 +32,7 @@ def log_and_check_normalizers(energy_normalizer, forces_normalizer, loaded=False
         bm_logging.info(f"Set normalizers from the checkpoint")
     else:
         bm_logging.info(f"Set normalizers by the config")
-    bm_logging.info(f" - energy uses {type(energy_normalizer).__name__} \
-                     (shift: {energy_normalizer.mean}, scale: {energy_normalizer.std})")
-    bm_logging.info(f" - forces uses {type(forces_normalizer).__name__} \
-                     (shift: {forces_normalizer.mean}, scale: {forces_normalizer.std})")
+    bm_logging.info(f" - energy uses {type(energy_normalizer).__name__} (shift: {energy_normalizer.mean}, scale: {energy_normalizer.std})")
+    bm_logging.info(f" - forces uses {type(forces_normalizer).__name__} (shift: {forces_normalizer.mean}, scale: {forces_normalizer.std})")
     assert forces_normalizer.mean == 0, "mean of forces should be zero"
     assert forces_normalizer.std  == energy_normalizer.std, "scales of energy and forces should be identical"
