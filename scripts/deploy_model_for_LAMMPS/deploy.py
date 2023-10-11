1884f92bb081ebfde5b31de1aeae1f8083b2a3cb
byunggook.na <byunggook.na@samsung.com>
2023-10-11 11:57:11 +0900

    move the deploying code into script directory

--- scripts/deploy_model_for_LAMMPS/deploy.py
+++ scripts/deploy_model_for_LAMMPS/deploy.py
@@ -1,3 +1,13 @@
+"""
+Copyright (C) 2023 Samsung Electronics Co. LTD
+
+This software is a property of Samsung Electronics.
+No part of this software, either material or conceptual may be copied or distributed, transmitted,
+transcribed, stored in a retrieval system or translated into any human or computer language in any form by any means,
+electronic, mechanical, manual or otherwise, or disclosed
+to third parties without the express written permission of Samsung Electronics.
+"""
+
 import sys
 import os
 sys.path.insert(0, os.path.abspath("./"))
@@ -59,7 +69,7 @@ elif sys.argv[1] == "Allegro":
     load_state_dict(model, new_dict, strict=True)
     model = compile_for_deploy(model.allegro_model)
 else:
-    raise RunError("other models are not supported")
+    raise RuntimeError("other models are not supported")
 
 # Deploy
 metadata = {}
@@ -67,19 +77,17 @@ metadata[TORCH_VERSION_KEY] = "1.12.1+cu116"
 metadata[E3NN_VERSION_KEY] = "0.5.1"
 metadata[NEQUIP_VERSION_KEY] = "0.5.6"
 metadata[R_MAX_KEY] = "6.0"
-if('chemical_symbols' in ckpt['config']['model_attributes'].keys()):
-    chemical_symbols=ckpt['config']['model_attributes']['chemical_symbols']
+if 'chemical_symbols' in ckpt['config']['model_attributes'].keys():
+    chemical_symbols = ckpt['config']['model_attributes']['chemical_symbols']
     atomic_nums = [ase.data.atomic_numbers[sym] for sym in chemical_symbols]
-    chemical_symbols = [
-                    e[1] for e in sorted(zip(atomic_nums, chemical_symbols))
-                ]
+    chemical_symbols = [e[1] for e in sorted(zip(atomic_nums, chemical_symbols))]
     type_names = chemical_symbols
-elif ('chemical_symbol_to_type' in ckpt['config']['model_attributes'].keys()):
-    symbols=list(ckpt['config']['model_attributes']['chemical_symbol_to_type'].keys())
-    vals=[ ckpt['config']['model_attributes']['chemical_symbol_to_type'][sym_] for sym_ in symbols]
-    idx=np.argsort(vals)
-    sym__=np.array(symbols)
-    type_names=list(sym__[idx])
+elif 'chemical_symbol_to_type' in ckpt['config']['model_attributes'].keys():
+    symbols = list(ckpt['config']['model_attributes']['chemical_symbol_to_type'].keys())
+    vals = [ckpt['config']['model_attributes']['chemical_symbol_to_type'][sym] for sym in symbols]
+    idx = np.argsort(vals)
+    sym_array = np.array(symbols)
+    type_names = list(sym_array[idx])
 metadata[N_SPECIES_KEY] = f"{len(type_names)}"
 metadata[TYPE_NAMES_KEY] = " ".join(type_names)
 metadata[JIT_BAILOUT_KEY] = "2"
