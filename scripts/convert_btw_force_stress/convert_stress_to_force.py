3344193a01088abb9ace3cec8d5e84a03382564b
byunggook.na <byunggook.na@samsung.com>
2023-10-11 11:35:16 +0900

    add license head and refine the code in converting scripts

--- scripts/convert_btw_force_stress/convert_stress_to_force.py
+++ scripts/convert_btw_force_stress/convert_stress_to_force.py
@@ -1,52 +1,77 @@
+"""
+Copyright (C) 2023 Samsung Electronics Co. LTD
+
+This software is a property of Samsung Electronics.
+No part of this software, either material or conceptual may be copied or distributed, transmitted,
+transcribed, stored in a retrieval system or translated into any human or computer language in any form by any means,
+electronic, mechanical, manual or otherwise, or disclosed
+to third parties without the express written permission of Samsung Electronics.
+"""
 
 import argparse
 import torch
 import yaml
 import os
 
-def convert(ckpt_path,out_path):
-    
-    ckpt_force=torch.load(os.path.join(ckpt_path,'checkpoint.pt'),'cpu')
-    ckpt_force['config']['model_attributes']['regress_stress']=False
-    ckpt_force['config']['model_attributes']['model_builders'][-2]='ForceOutput'
-    ckpt_force['config']['task']['metrics']=['energy_per_atom_mae', 'energy_per_atom_mse', 'forces_mae', 'forces_mse']
-    
-    for k in ckpt_force['state_dict'].keys():
 
-        if(k.startswith("module.module")):
-            ckpt_force['state_dict'].pop(".".join(k.split(".")[:3])+'.model._empty')
+def convert_builders(model_builders):
+    builders = []
+    for builder in model_builders:
+        if builder == "StressForceOutput":
+            builders.append("ForceOutput")
         else:
-            ckpt_force['state_dict'].pop(".".join(k.split(".")[:2])+'.model._empty')
-        break
-    
-    with open (os.path.join(ckpt_path,'config_train.yml'),'r')as f:
-        config=yaml.load(f,yaml.FullLoader)
-    config['model']['regress_stress']=False
-    config['model']['model_builders'][-2]='ForceOutput'
-    config['task']['metrics'] =['energy_per_atom_mae', 'energy_per_atom_mse', 'forces_mae', 'forces_mse']
-    config['use_stress']=False
-    os.makedirs(out_path,exist_ok=True)
+            builders.append(builder)
+    return builders
 
-    torch.save(ckpt_force,os.path.join(out_path,"checkpoint.pt"))
-    with open (os.path.join(out_path,'config_train.yml'),'w')as f:
-        yaml.dump(config,f)
+
+def convert(ckpt_path, ckpt_name, out_path):
+    # modify the config information saved in the checkpoint
+    ckpt = torch.load(os.path.join(ckpt_path, ckpt_name), map_location='cpu')
+    ckpt['config']['model_attributes']['regress_stress'] = False
+    ckpt['config']['model_attributes']['model_builders'] = convert_builders(ckpt['config']['model_attributes']['model_builders'])
+    ckpt['config']['task']['metrics'] = ['energy_per_atom_mae', 'energy_per_atom_mse', 'forces_mae', 'forces_mse']
+    for k in ckpt['state_dict'].keys():
+        if k.startswith("module.module"):
+            del_key = ".".join(k.split(".")[:3]) + '.model._empty'
+        else:
+            del_key = ".".join(k.split(".")[:2]) + '.model._empty'
+        break
+    ckpt['state_dict'].pop(del_key)
+    
+    # modify the config file saved in the directory
+    with open(os.path.join(ckpt_path, 'config_train.yml'),'r') as f:
+        config = yaml.load(f, yaml.FullLoader)
+    config['model']['regress_stress'] = False
+    config['model']['model_builders'] = convert_builders(config['model']['model_builders'])
+    config['task']['metrics'] = ['energy_per_atom_mae', 'energy_per_atom_mse', 'forces_mae', 'forces_mse']
+    config['use_stress'] = False
+    os.makedirs(out_path, exist_ok=True)
+
+    # save the results
+    torch.save(ckpt, os.path.join(out_path, ckpt_name))
+    with open(os.path.join(out_path, 'config_train.yml'), 'w') as f:
+        yaml.dump(config, f)
 
 
 if __name__ == "__main__":
-
-
-    parser = argparse.ArgumentParser(description="Converting ForceNequip to StressForceNequip")
+    parser = argparse.ArgumentParser(description="Converting StressForceOutput to ForceOutput")
     parser.add_argument(
-            "--ckpt-dir",
-            type=str,
-            default=None,
-            help="Path of trained checkpoint folder",
-        )
+        "--ckpt-dir",
+        type=str,
+        default=None,
+        help="Path of trained checkpoint directory",
+    )
     parser.add_argument(
-            "--out-dir",
-            type=str,
-            default=None,
-            help="Path of converted output checkpoint folder",
-        )
-    args=parser.parse_args()
-    convert(args.ckpt_dir,args.out_dir)
+        "--ckpt-name",
+        type=str,
+        default='checkpoint.pt',
+        help="Filename of the trained checkpoint (default: checkpoint.pt)",
+    )
+    parser.add_argument(
+        "--out-dir",
+        type=str,
+        default=None,
+        help="Path of converted output checkpoint directory",
+    )
+    args = parser.parse_args()
+    convert(args.ckpt_dir, args.ckpt_name, args.out_dir)
